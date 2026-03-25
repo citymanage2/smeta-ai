34e22b9822763b3e291e43e9400671e5403b4ad7
citymanage2 <zlatkovskiij@gmail.com>
2026-03-26 03:34:50 +0400

    feat: add Project model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.user import User
 from app.models.task import Task
 from app.models.result import TaskResult
 from app.models.price import PriceWork, PriceMaterial
+from app.models.project import Project
 
-__all__ = ["User", "Task", "TaskResult", "PriceWork", "PriceMaterial"]
+__all__ = ["User", "Task", "TaskResult", "PriceWork", "PriceMaterial", "Project"]
